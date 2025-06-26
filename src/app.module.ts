5beb996e1637b549620efbb75335455028de77bf
esertec61 <esertec61@gmail.com>
2025-06-25 20:53:58 -0400

    complete crud usuarios

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { EstadosModule } from './feature/estados/estados.module';
 import { UsuariosModule } from './feature/usuarios/usuarios.module';
+import { ProductosModule } from './feature/productos/productos.module';
 
 @Module({
-  imports: [EstadosModule, UsuariosModule],
+  imports: [EstadosModule, UsuariosModule, ProductosModule],
   controllers: [AppController],
   providers: [AppService],
 })
