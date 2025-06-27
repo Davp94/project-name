ec9aa94931ea6576991b73fefd68ef84d06699f9
david_blumbit <david.villca.pacheco@gmail.com>
2025-06-26 20:42:23 -0400

    add changes controller app

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -7,6 +7,13 @@ export class AppController {
 
   @Get()
   getHello(): string {
+    const saludo = 'Hola, mundo!';
+    console.log(saludo);
+    // Aquí puedes realizar cualquier otra lógica que necesites
+    // Por ejemplo, llamar a un servicio o manipular datos
+    // En este caso, simplemente retornamos el saludo
+    // a través del servicio AppService
+    //this.appService.logMessage(saludo);
     return this.appService.getHello();
   }
 }
